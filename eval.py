dcc84963cd56cb134bdc548a255ff002515412e4
Matthew <mmaillet@hamilton.edu>
2025-03-24 14:15:48 -0400

    update onnx_helper
    conformed onnx_helper to the standard we need for eval.py to run successfully

--- eval.py
+++ eval.py
@@ -8,6 +8,8 @@ import argparse
 from sim_data import defaultDataset
 
 from transformer_model import DeepFakeDetectorTransformer
+from onnx_helper import DeepFakeModel
+
 
 def args_func():
     parser = argparse.ArgumentParser()
@@ -17,7 +19,7 @@ def args_func():
         default="datasets/test",
         help="Path to the dataset folder.",
     )
-    #TODO add arguments for models to use
+    # TODO add arguments for models to use
     args = parser.parse_args()
     return args
 
@@ -29,14 +31,16 @@ def get_area_ratio(img1, img2):
     return (h1 * w1) / (h2 * w2)
 
 
-#Inputs: models (list of model objects), dataset (dataset object)
-#Outputs: results (a list of lists of dictionaries, one for each model)
+# Inputs: models (list of model objects), dataset (dataset object)
+# Outputs: results (a list of lists of dictionaries, one for each model)
 def run_models(models, dataset):
     results = []
     for model in models:
         model_results = []
         model_results.append({"model_name": model.__class__.__name__})
-        for i in range(len(dataset)): #This is done one image at a time to avoid memory issues
+        for i in range(
+            len(dataset)
+        ):  # This is done one image at a time to avoid memory issues
             sample = dataset[i]
             image = sample["image"]
             image_path = sample["image_path"]
@@ -51,7 +55,7 @@ def run_models(models, dataset):
             # Postprocess the prediction
             processed_prediction = model.postprocess(prediction)
 
-            #Add the name of the image to the prediction
+            # Add the name of the image to the prediction
             processed_prediction["image_path"] = image_path
 
             # Append the result to the list
@@ -69,13 +73,11 @@ if __name__ == "__main__":
     input = "sample_input/real"
 
     args = args_func()
-    #input = args.dataset_path
+    # input = args.dataset_path
 
-    test_dataset = defaultDataset(
-        dataset_path=input, resolution=224
-    )
+    test_dataset = defaultDataset(dataset_path=input, resolution=224)
 
-    models_to_use = [DeepFakeDetectorTransformer()]
+    models_to_use = [DeepFakeModel("onnx_models/deepfake_image_model.onnx")]
 
     results = run_models(models_to_use, test_dataset)
 
--- onnx_helper.py
+++ onnx_helper.py
@@ -6,14 +6,14 @@ import onnxruntime as ort
 import numpy as np
 
 
-class DeepFakeProcessing:
-    def __init__(self, resolution=224):
+class DeepFakeModel:
+    def __init__(self, model_path, resolution=224):
+        self.session = ort.InferenceSession(
+            model_path,
+            providers=["CUDAExecutionProvider", "CPUExecutionProvider"],
+        )
         self.resolution = resolution
         self.valid_extensions = (".jpg", ".jpeg", ".png")
-
-    def apply_transforms_on_path(self, path):
-        image = Image.open(path).convert("RGB")
-        return self.apply_transforms(image)
 
     def apply_transforms(self, image):
         return T.Compose(
@@ -29,51 +29,18 @@ class DeepFakeProcessing:
             None,
         ].numpy()
 
-    def find_images_in_dir(self, directory):
-        return [
-            os.path.join(directory, f)
-            for f in os.listdir(directory)
-            if os.path.isfile(os.path.join(directory, f))
-            and f.lower().endswith(self.valid_extensions)
-        ]
-
-    def preprocess(self, dir_path):
-        return [
-            self.preprocess_single(path) for path in self.find_images_in_dir(dir_path)
-        ]
-
-    def preprocess_single(self, image_path):
-        return self.apply_transforms_on_path(image_path)
+    def preprocess(self, image):
+        return self.apply_transforms(image)
 
     def decode_prediction(self, prediction):
         conf = prediction if prediction > 0.5 else 1 - prediction
         pred = "real" if prediction > 0.8 else "fake" if conf > 0.8 else "uncertain"
         return {"prediction": pred, "confidence": conf.item()}
 
-    def postprocess(self, outputs):
-        return [self.postprocess_single(out) for out in outputs]
-
-    def postprocess_single(self, output):
+    def postprocess(self, output):
+        print(output)
         return self.decode_prediction(torch.sigmoid(torch.tensor(output[0][0].item())))
 
-
-class DeepFakeModel:
-    def __init__(self, model_path):
-        self.dfp = DeepFakeProcessing()
-        self.session = ort.InferenceSession(
-            model_path,
-            providers=["CUDAExecutionProvider", "CPUExecutionProvider"],
-        )
-
-    def predict(self, image_path):
-        input = self.dfp.preprocess_single(image_path)
+    def predict(self, input):
         output = self.session.run(None, {"input": input})
-        out = self.dfp.postprocess_single(output[0])
-        out["image_path"] = image_path
-        return out
-
-    def predict_dir(self, input_dir):
-        outputs = []
-        for inp in self.dfp.find_images_in_dir(input_dir):
-            outputs.append(self.predict(inp))
-        return outputs
+        return output[0]
