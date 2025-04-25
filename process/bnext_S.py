d8cc5921ef85e96e81dbc7dfdf9bd2a1372cf3d3
zerOsand <mmaillet@hamilton.edu>
2025-04-24 21:23:22 -0400

    added utils.py, fix bnext_M pathlib, update bnext_M and S apply_transforms

--- process/bnext_S.py
+++ process/bnext_S.py
@@ -2,7 +2,7 @@ from PIL import Image
 import onnxruntime as ort
 import numpy as np
 from pathlib import Path
-
+from process.utils import Compose, InterpolationMode, Resize, CenterCrop, ToImage, ToDtype
 
 # Trained on COCOFake dataset
 class BNext_S_ModelONNX:
@@ -17,20 +17,16 @@ class BNext_S_ModelONNX:
         self.resolution = resolution
         self.valid_extensions = (".jpg", ".jpeg", ".png")
 
-    def apply_transforms(self, image):
-        size = self.resolution + self.resolution // 8
-        image = image.resize((size, size), resample=Image.BILINEAR)
-        left = (size - self.resolution) // 2
-        top = (size - self.resolution) // 2
-        right = left + self.resolution
-        bottom = top + self.resolution
-        image = image.crop((left, top, right, bottom))
-        arr = np.array(image).astype(np.float32) / 255.0
-        if arr.ndim == 2:  # grayscale => add channel dimension
-            arr = np.expand_dims(arr, axis=-1)
-        arr = np.transpose(arr, (2, 0, 1))
-
-        return arr[None, ...]
+    def apply_transforms(self, image: Image.Image) -> np.ndarray:
+        transform = Compose([
+            Resize(self.resolution + self.resolution // 8, interpolation=InterpolationMode.BILINEAR),
+            CenterCrop(self.resolution),
+            ToImage(),
+            ToDtype(np.float32, scale=True),
+        ])
+        out = transform(image)           # H×W×C float32 in [0,1]
+        out = out.transpose(2, 0, 1)
+        return out[None, ...]           # add batch dim
 
     def preprocess(self, image):
         return self.apply_transforms(image)
